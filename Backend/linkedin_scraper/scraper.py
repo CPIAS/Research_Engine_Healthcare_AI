a3c27029a03da30b015485754eab36fb54cda343
adam-360 <adam.360@hotmail.fr>
2023-11-01 01:45:01 -0400

    added profile formatter

--- Backend/linkedin_scraper/scraper.py
+++ Backend/linkedin_scraper/scraper.py
@@ -1,10 +1,12 @@
-from linkedin_scraper import Person, actions
 from selenium import webdriver
 from dotenv import load_dotenv
 import pandas as pd
 import os
+from linkedin_scraper import Person, actions
+from profile_formatter import profile_to_dict
+import json
 
-df = pd.read_csv('LinkedIn_Profiles.csv')
+df = pd.read_csv('LinkedIn_Profiles_short_ver.csv')
 linkedIn_list = df.LinkedIn
 load_dotenv()
 driver = webdriver.Chrome()
@@ -14,26 +16,23 @@ password = os.environ['LINKEDIN_PASSWORD']
 actions.login(driver, email, password)  # if email and password isnt given, it'll prompt in terminal
 
 linkedin_profiles = []
-
+json_profiles = {
+    "profiles": {}
+}
+unique_id = 0
 for link in linkedIn_list:
+    unique_id += 1
     try:
         person = Person(link, driver=driver, scrape=True, close_on_complete=False)
+        print(person.experiences[0])
     except Exception as e:
         print(e)
         continue
+    json_profiles['profiles'][unique_id] = profile_to_dict(person)
     linkedin_profiles.append(person)
 print(len(linkedin_profiles))
-
-columns = ['LinkedIn URL', 'Name', 'About', 'Experiences', 'Interests', 'Accomplishments',
-           'Company', 'Job Title', 'Contacts']
-
-
-def profile_to_dict(profile):
-    return {'url': profile.linkedin_url, 'Name': profile.name, 'About': profile.about,
-            'Experiences': profile.experiences, 'Interests': profile.interests,
-            'Accomplishments': profile.accomplishments, 'Company': profile.company,
-            'Job Title': profile.job_title, 'Contacts': profile.contacts}
+print(json_profiles)
+with open("bug_fixing.json", "w") as outfile:
+    json.dump(json_profiles, outfile, indent=4, default=lambda o: '<not serializable>')
 
 
-df = pd.DataFrame([profile_to_dict(x) for x in linkedin_profiles], columns=columns)
-df.to_csv('all_extracted_profiles.csv', index=True, encoding='utf-8')
